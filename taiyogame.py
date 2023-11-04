dbfa4ed064c9d5755b3d9fd98a508173f3fba513
Eric <eric3691215@gmail.com>
2023-11-04 12:44:47 -0400

    ball class multiple balls

--- taiyogame.py
+++ taiyogame.py
@@ -10,33 +10,74 @@ draw_options = pymunk.pygame_util.DrawOptions(screen)
 
 # Create a space and set the gravity
 space = pymunk.Space()
-space.gravity = (0, -981)  # Negative since y goes down in most renderers
+space.gravity = (0, 981)  # Negative since y goes down in most renderers
+
+# ball image
+radius = 20
+ball_image = pygame.image.load('images/sun.png')
+ball_image = pygame.transform.scale(ball_image, (radius * 2, radius * 2))  # The image is scaled to 2x the radius of the ball
 
 # Create static lines to form a U-shape
 static_lines = [
-    pymunk.Segment(space.static_body, (150, 100), (150, 300), 5),
-    pymunk.Segment(space.static_body, (150, 100), (450, 100), 5),
-    pymunk.Segment(space.static_body, (450, 300), (450, 100), 5)
+    pymunk.Segment(space.static_body, (150, 300), (150, 500), 5),
+    pymunk.Segment(space.static_body, (150, 500), (450, 500), 5),
+    pymunk.Segment(space.static_body, (450, 500), (450, 300), 5)
 ]
 for line in static_lines:
     line.elasticity = 0.95  # To make the ball bounce a bit
     space.add(line)
 
+class Ball:
+    def __init__(self, space, position, mass, radius, image_path, scale_factor=1):
+        self.radius = radius
+        self.is_resting = False
+        
+        # Pymunk physics setup
+        moment = pymunk.moment_for_circle(mass, 0, self.radius)
+        self.body = pymunk.Body(mass, moment)
+        self.body.position = position
+        self.shape = pymunk.Circle(self.body, self.radius)
+        self.shape.elasticity = 0.95
+        space.add(self.body, self.shape)
+        
+        # Load the image for the ball
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (int(self.radius * 2 * scale_factor), int(self.radius * 2 * scale_factor)))
+        
+    def update(self, dt):
+        # The physical position will be updated by the Pymunk space.step() method
+        pass
+    
+    def draw(self, screen):
+        # Get the position for Pygame (adjust for the coordinate system if needed)
+        position = self.body.position.x, self.body.position.y  # No flipping of y-coordinate in this example
+        
+        # Get a rect structure from the image
+        rect = self.image.get_rect(center=position)
+        
+        # Blit the image onto the screen
+        screen.blit(self.image, rect.topleft)
+
 # Create a dynamic ball
-mass = 1
-radius = 25
-moment = pymunk.moment_for_circle(mass, 0, radius)  # 1st argument is mass, 2nd is inner radius, 3rd is outer radius
-ball_body = pymunk.Body(mass, moment)
-ball_body.position = (300, 400)  # Starting position above the U-shape
-ball_shape = pymunk.Circle(ball_body, radius)
-ball_shape.elasticity = 0.95
-space.add(ball_body, ball_shape)
+# mass = 1
+# radius = 25
+# moment = pymunk.moment_for_circle(mass, 0, radius)  # 1st argument is mass, 2nd is inner radius, 3rd is outer radius
+# ball_body = pymunk.Body(mass, moment)
+# ball_body.position = (300, 200)  # Starting position above the U-shape
+# ball_shape = pymunk.Circle(ball_body, radius)
+# ball_shape.elasticity = 0.95
+# space.add(ball_body, ball_shape)
+
+balls = [
+    Ball(space, (300, 200), 1, 25, "images/sun.png"),
+    Ball(space, (280, 100), 1, 25, "images/venus.png"),
+]
 
 # Simulation loop
 for i in range(300):
     # Step the simulation
     space.step(1 / 50.0)
-    print(ball_body.position)  # Optionally print the position of the ball
+    # print(ball_body.position)  #a Optionally print the position of the ball
 
 # Main loop
 running = True
@@ -47,6 +88,18 @@ while running:
 
     screen.fill((255, 255, 255))  # Fill the screen with white background
     space.debug_draw(draw_options)  # Draw the space with the debug_draw util
+
+    for ball in balls:
+        ball.update(1 / 50.0)
+
+    # Draw the balls
+    for ball in balls:
+        ball.draw(screen)
+
+    # ball_position = int(ball_body.position.x), int(ball_body.position.y)  # Flip the y-coordinate for Pygame
+    # ball_rect = ball_image.get_rect(center=ball_position)
+    # screen.blit(ball_image, ball_rect)
+
     pygame.display.flip()  # Update the full display Surface to the screen
 
     space.step(1 / 50.0)  # Step the simulation
