3074d64a490dc3bc2cf7737a597c2617d5b90930
Eric <eric3691215@gmail.com>
2023-11-05 01:45:08 -0400

    freezes before second run

--- agent.py
+++ agent.py
@@ -74,7 +74,7 @@ def train():
     just_started = True 
     done = False
     while True:
-
+        
         if not velocity_zero:
             reward, done, score, velocity_zero = game.run_game()
             reward_since_action += reward
@@ -107,11 +107,11 @@ def train():
             print('Game', agent.n_games, 'Score', score, 'Record:', record)
 
             # plotting stuff if we want
-            plot_scores.append(score)
-            total_score += score
-            mean_score = total_score / agent.n_games
-            plot_mean_scores.append(mean_score)
-            plot(plot_scores, plot_mean_scores)
+            # plot_scores.append(score)
+            # total_score += score
+            # mean_score = total_score / agent.n_games
+            # plot_mean_scores.append(mean_score)
+            # plot(plot_scores, plot_mean_scores)
 
 if __name__ == '__main__':
     train()
